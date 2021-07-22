7bc7fb3e73c4063111d15ea63e8f307fa4bfd6c0
Shaon Baidya <kenkaneki0625@gmail.com>
2021-07-22 17:21:10 +0530

    basic up and routing

---
+++ meetup-nextjs/pages/index.js
@@ -0,0 +1,29 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        key : '1',
+        id : 'm1',
+        image : 'https://us.123rf.com/450wm/vichie81/vichie811508/vichie81150800164/44510493-grand-place-brussels-belgium-at-dusk-.jpg?ver=6',
+        title : 'A',
+        address : 'aa',
+    },
+    {
+        key : '2',
+        id : 'm2',
+        image : 'https://us.123rf.com/450wm/vichie81/vichie811508/vichie81150800164/44510493-grand-place-brussels-belgium-at-dusk-.jpg?ver=6',
+        title : 'B',
+        address : 'bb',
+    },
+    {
+        key : '3',
+        id : 'm3',
+        image : 'https://us.123rf.com/450wm/vichie81/vichie811508/vichie81150800164/44510493-grand-place-brussels-belgium-at-dusk-.jpg?ver=6',
+        title : 'C',
+        address : 'cc',
+    },
+]
+
+export default function HomePage(){
+    return <MeetupList meetups={DUMMY_MEETUPS}/>
+}
---
+++ meetup-nextjs/pages/new-meetup.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+    function addMeetupHandler (data) {
+        console.log(data)
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
