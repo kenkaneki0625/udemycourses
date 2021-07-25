f7b5bae6f683110b8ad9db5fc552987548e5dc15
Shaon Baidya <kenkaneki0625@gmail.com>
2021-07-25 16:41:51 +0530

    getStaticPaths implementation

--- meetup-nextjs/pages/[meetupId].js
+++ meetup-nextjs/pages/[meetupId].js
@@ -12,3 +12,39 @@ export default function MeetupDetails(){
         />
     )
 }
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+            params:{
+                meetupId: 'm1'
+            }
+        },
+        {
+            params:{
+                meetupId: 'm2'
+            }
+        },
+        
+    ]
+    }
+}
+
+export async function getStaticProps(context){
+
+    const meetupId = context.params.meetupId
+    return {
+        props:{
+            meetupData : {
+                id : meetupId,
+                image : "https://us.123rf.com/450wm/vichie81/vichie811508/vichie81150800164/44510493-grand-place-brussels-belgium-at-dusk-.jpg?ver : 6",
+                title : 'first meetup',
+                address : 'some street',
+                description : 'Some description',
+            }
+        },
+        revalidate : 3600
+    }
+}
